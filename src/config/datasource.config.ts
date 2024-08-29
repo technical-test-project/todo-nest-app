45e23ec052f80a97d307b477896fc82a3b66383d
Alvin Setiawan <alvin.set25@gmail.com>
2024-08-29 15:41:30 +0700

    Update datasource.config.ts

--- src/config/datasource.config.ts
+++ src/config/datasource.config.ts
@@ -7,5 +7,15 @@ config();
 const configService = new ConfigService();
 
 export default new DataSource({
-  ...configService.get('databaseConfig'),
+  type: 'mysql',
+  host: configService.get('DB_HOST'),
+  port: parseInt(configService.get('DB_PORT'), 10),
+  username: configService.get('DB_USERNAME'),
+  password: configService.get('DB_PASSWORD'),
+  database: configService.get('DB_DATABASE'),
+  synchronize: configService.get('NODE_ENV') === 'development',
+  logging: configService.get('NODE_ENV') === 'development',
+  // entities: [`${__dirname}/../**/*.entity{.ts,.js}`],
+  migrations: [`${__dirname}/../database/migrations/*{.ts,.js}`],
+  // migrationsTableName: 'migrations',
 });
